caf71780891057a0658208024832e012120b4a20
Dave Hall <dave@etianen.com>
2012-11-02 15:40:17 +0000

    Final boilerplate for 0.9 release.

--- setup.py
+++ setup.py
@@ -1,44 +1,10 @@
-import os
-
 from distutils.core import setup
 
-from require import __version__
+from herokuapp import __version__
 
 
 version_str = ".".join(str(n) for n in __version__)
-
-
-def fullsplit(path, result=None):
-    """
-    Split a pathname into components (the opposite of os.path.join) in a
-    platform-neutral way.
-    """
-    if result is None:
-        result = []
-    head, tail = os.path.split(path)
-    if head == '':
-        return [tail] + result
-    if head == path:
-        return result
-    return fullsplit(head, [tail] + result)
-
-
-# Compile the list of packages available, because distutils doesn't have
-# an easy way to do this.
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir != '':
-    os.chdir(root_dir)
-
-for dirpath, dirnames, filenames in os.walk("herokuapp"):
-    # Ignore dirnames that start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'): del dirnames[i]
-    if '__init__.py' in filenames:
-        packages.append('.'.join(fullsplit(dirpath)[1:]))
-    elif filenames:
-        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
-
+        
 
 setup(
     name = "django-herokuapp",
@@ -49,8 +15,24 @@ setup(
     author_email = "dave@etianen.com",
     url = "https://github.com/etianen/django-herokuapp",
     download_url = "https://github.com/downloads/etianen/django-herokuapp/django-herokuapp-{}.tar.gz".format(version_str),
-    packages = packages,
-    data_files = data_files,
+    packages = [
+        "herokuapp",
+        "herokuapp.bin",
+    ],
+    package_data = {
+        "herokuapp": [
+            "project_template/*.py",
+            "project_template/*.conf",
+            "project_template/*.txt",
+            "project_template/Procfile",
+            "project_template/project_name/*.py",
+            "project_template/project_name/templates/.gitignore",
+            "project_template/project_name/static/js/.gitignore",
+            "project_template/project_name/apps/*.py",
+            "project_template/project_name/settings/*.py",
+            "project_template/project_name/static/*.py",
+        ],
+    },
     classifiers = [
         "Development Status :: 4 - Beta",
         "Environment :: Web Environment",
