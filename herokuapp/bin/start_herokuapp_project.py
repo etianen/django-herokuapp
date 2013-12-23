620bfd5d0a8ad4771b3e7af8e597b88f2bf2b803
Dave Hall <dave@etianen.com>
2013-12-23 15:34:42 +0000

    Creating requirements file dynamically

--- herokuapp/bin/start_herokuapp_project.py
+++ herokuapp/bin/start_herokuapp_project.py
@@ -2,6 +2,8 @@ import sys, os.path, getpass
 
 from django.core import management
 
+import subprocess
+
 
 def main():
     argv = list(sys.argv)
@@ -10,9 +12,13 @@ def main():
     project_name = argv[1]
     management.call_command("startproject",
         project_name,
+        ".",
         template = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "project_template")),
         extensions = ("py", "txt", "slugignore", "conf", "gitignore,sh",),
         files = ("Procfile",),
         app_name = project_name.replace("_", "-"),
         user = getpass.getuser(),
     )
+    # Freeze project requirements.
+    with open("requirements.txt", "wb") as requirements_handle:
+        subprocess.call(["pip", "freeze"], stdout=requirements_handle)
