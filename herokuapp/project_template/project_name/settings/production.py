6300fda25fcafd32fbe274fc119854e5662c1b04
Dave Hall <dave@etianen.com>
2013-12-30 15:21:01 +0000

    Using new template context processors defaults

--- herokuapp/project_template/project_name/settings/production.py
+++ herokuapp/project_template/project_name/settings/production.py
@@ -190,8 +190,9 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.i18n",
     "django.core.context_processors.media",
     "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+#    "django.core.context_processors.request",
     "django.contrib.messages.context_processors.messages",
-    "django.core.context_processors.request",
 )
 
 
