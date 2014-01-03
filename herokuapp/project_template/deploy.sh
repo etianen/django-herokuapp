d4e380ae5582fce90bfd92de5512676aea3934e5
Dave Hall <dave@etianen.com>
2014-01-03 13:13:49 +0000

    Using Heroku API key for login user username and password

--- herokuapp/project_template/deploy.sh
+++ herokuapp/project_template/deploy.sh
@@ -13,19 +13,14 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # A CI server typically starts from a zero setup with no
 # Heroku toolbelt, and no authenticated Heroku user. This
 # section bootstraps the Heroku environment.
+# A HEROKU_API_KEY environmental variable should be provided
+# by the CI server for secure authentication.
 if [[ "$CI" == "true" ]]
 then
 # If the Heroku toolbelt is not installed, install it.
 command -v heroku >/dev/null 2>&1 || { 
     wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 }
-# Attempt to login to Heroku programatically. This assumes
-# that the HEROKU_USER and HEROKU_PASSWORD environmental variables
-# have been set securely via the CI config.
-if [[ "$HEROKU_USER" != "" ]] && [[ "$HEROKU_PASSWORD" != "" ]]
-then
-    printf "$HEROKU_USER\n$HEROKU_PASSWORD" | heroku login
-fi
 # Download the Heroku config, if no .env file is present.
 if [ ! -f $DIR/.env ]
 then
